54f1d93d4bbe3d742de40c7118e3feb51eaa3960
DESKTOP-SF55LPR\leo <leonid1995@ukr.net>
2020-04-23 00:36:06 +0300

    v1.3
    scrol and anchor

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
-var slideIndex = 0;
+let slideIndex = 0;
 showSlides();
 
 function showSlides() {
-    var i;
-    var slides = document.getElementsByClassName("mySlides");
+    let i;
+    let slides = document.getElementsByClassName("mySlides");
     for (i = 0; i < slides.length; i++) {
         slides[i].style.display = "none";
     }
@@ -12,3 +12,16 @@ function showSlides() {
     slides[slideIndex-1].style.display = "block";
     setTimeout(showSlides, 10000); // Change image every 2 seconds
 }
+
+
+
+$(document).ready(function() {
+    $("a.scrollto").click(function() {
+        var elementClick = $(this).attr("href")
+        var destination = $(elementClick).offset().top;
+        jQuery("html:not(:animated),body:not(:animated)").animate({
+            scrollTop: destination
+        }, 800);
+        return false;
+    });
+});
